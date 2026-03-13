02aa4114a63e4b80bbde7bca0740e5cad2742321
Mahi Sahu <154459874+Mahi-Sahu@users.noreply.github.com>
2026-03-13 22:18:31 +0530

    Add files via upload

---
+++ Day1Spring_13_03_26/SpringAssignment13_02_26UsingConstructorArgs/src/main/java/com/cg/bean/Employee.java
@@ -0,0 +1,86 @@
+package com.cg.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+    private int empid;
+    private String empname;
+    private double salary;
+    private String bu;
+    private int age;
+    private SBU sbu;
+
+    public Employee(){}
+
+    public Employee(int empid, String empname, double salary, String bu, int age, SBU sbu) {
+        this.empid = empid;
+        this.empname = empname;
+        this.salary = salary;
+        this.bu = bu;
+        this.age = age;
+        this.sbu = sbu;
+    }
+
+    public SBU getSbu() {
+        return sbu;
+    }
+
+    public void setSbu(SBU sbu) {
+        this.sbu = sbu;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getBu() {
+        return bu;
+    }
+
+    public void setBu(String bu) {
+        this.bu = bu;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void employeeDeatils(){
+        System.out.println("Employee ID: "+empid);
+        System.out.println("Employee Name: "+empname);
+        System.out.println("Employee Salary: "+salary);
+        System.out.println("Employee Bu: "+bu);
+        System.out.println("Employee Age: "+age);
+    }
+
+    public void showEmployeeSBUDetails(){
+        System.out.println("Employee Details");
+        System.out.println("---------------------");
+        System.out.println("Employee [empName="+empname+", empId="+empid+", empSalary="+salary
+        +", sbu details= SBU[ sbuCode="+sbu.getSbuCode()+", sbuHead="+sbu.getSbuHead()+", sbuName="+sbu.getSbuName()+"]]");
+    }
+}
---
+++ Day1Spring_13_03_26/SpringAssignment13_02_26UsingConstructorArgs/src/main/java/com/cg/bean/SBU.java
@@ -0,0 +1,59 @@
+package com.cg.bean;
+
+import java.util.List;
+
+public class SBU {
+    final private String sbuCode="PES-BU";
+    private int sbuId;
+    private String sbuName;
+    private String sbuHead;
+    private List<Employee> empList;
+
+    public int getSbuId() {
+        return sbuId;
+    }
+
+    public List<Employee> getEmpList() {
+        return empList;
+    }
+
+    public void setEmpList(List<Employee> empList) {
+        this.empList = empList;
+    }
+
+    public String getSbuCode() {
+        return sbuCode;
+    }
+
+    public void setSbuId(int sbuId) {
+        this.sbuId = sbuId;
+    }
+
+    public String getSbuName() {
+        return sbuName;
+    }
+
+    public void setSbuName(String sbuName) {
+        this.sbuName = sbuName;
+    }
+
+    public String getSbuHead() {
+        return sbuHead;
+    }
+
+    public void setSbuHead(String sbuHead) {
+        this.sbuHead = sbuHead;
+    }
+
+    public void showSbuDetails(){
+        System.out.println("SBU Details");
+        System.out.println("---------------");
+        System.out.println("sbuCode="+sbuCode+", sbuName="+sbuName+", sbuHead="+sbuHead);
+        System.out.println("Employee Deatils:----------");
+        System.out.print("Employee [");
+        for(Employee emp:empList){
+            emp.employeeDeatils();
+            System.out.print("]\n");
+        }
+    }
+}
