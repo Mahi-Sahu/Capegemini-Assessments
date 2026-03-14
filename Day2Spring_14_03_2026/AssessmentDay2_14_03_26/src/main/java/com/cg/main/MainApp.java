b5853389363dfbf891f842552202dfd43268cbac
Mahi Sahu <154459874+Mahi-Sahu@users.noreply.github.com>
2026-03-14 14:26:55 +0530

    Add files via upload

---
+++ Day2Spring_14_03_2026/AssessmentDay2_14_03_26/src/main/java/com/cg/main/MainApp.java
@@ -0,0 +1,21 @@
+package com.cg.main;
+
+import com.cg.bean.Librarian;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        //setter injection:
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        Librarian librarian = (Librarian) context.getBean("lib");
+        librarian.issueBook();
+
+        System.out.println("----------------------------");
+        //constructor injection:
+        ApplicationContext ctx= new ClassPathXmlApplicationContext("beanConstructor.xml");
+        Librarian librarian2 = (Librarian) ctx.getBean("lib");
+        librarian2.issueBook();
+
+    }
+}
